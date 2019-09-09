6032fa3bcaee9af6054af3729909710522f44d5e
BrenerQuevedo <brener.quevedo@ccc.ufcg.edu.br>
2019-09-09 17:39:47 -0300

    Incrementando o sistema de batalha

--- menu.cpp
+++ menu.cpp
@@ -1,20 +1,31 @@
 #include <bits/stdc++.h>
 #include <time.h>
-#include "persistence.h"
+
+
 using namespace std;
+
+void sleepcp();
+void actionFase1(struct JOGADOR jogador);
+void fase1(struct JOGADOR jogador);
+void fase2(struct JOGADOR jogador);
+void batalha(struct JOGADOR jogador, struct INIMIGO inimigo, int iniciativa);
+bool fugir();
+int rolaDado();
+string tentaFugir(bool flag);
+bool iniciativa(int iniciativa, int eIniciativa);
+
 
 struct JOGADOR {
     string nome;
     int vida = 150;
-    int dano = 10;
+    int dano = 20;
 };
 
 struct INIMIGO {
-    string nome;
+    string tipo;
     int vida;
     int dano;
 };
-
 
 void sleepcp(int milliseconds) {
     clock_t tempo_final;
@@ -60,9 +71,43 @@ void lobo() {
     printf("###  /                                      ; ###############\n");
     printf("#                                            ################\n");
 
-    sleepcp(2500);
+    sleepcp(1500);
     system("clear");
 }
+
+void imperator() {
+    printf("\n");                                        
+    printf("                      ==(W{==========-      /===-\n");
+    printf("                          ||  (.--.)         /===-_---~~~~~~~~~------____\n");
+    printf("                          | \_,|**|,__      |===-~___                _,-' `\n");
+    printf("             -==\        `\ ' `--'   ),    `//~\   ~~~~`---.___.-~~\n");
+    printf("         ______-==|        /`\_. .__/\ \    | |  \           _-~`\n");
+    printf("   __--~~~  ,-/-==\      (   | .  |~~~~|   | |   `\        ,'\n");
+    printf("_-~       /'    |  \     )__/==0==\\<>/   / /      \      /\n");
+printf("  .'        /       |   \      /~\___/~~\/  /' /        \   /'\n");
+printf(" /  ____  /         |    \`\.__/-~~   \  |_/'  /          \/'\n");
+printf("/-'~    ~~~~~---__  |     ~-/~         ( )   /'        _--~`\n");
+    printf("              \_|      /        _) | ;  ),   __--~~\n");
+    printf("                '~~--_/      _-~/- |/ \   '-~ \n");
+    printf("               {\__--_/}    /  \_>-|)<__\      \n");
+    printf("               /'   (_/  _-~  | |__>--<__|      |\n");
+    printf("              |   _/) )-~     | |__>--<__|      |\n");
+    printf("              / /~ ,_/       / /__>---<__/      |\n");
+    printf("             o-o _//        /-~_>---<__-~      /\n");
+    printf("             (^(~          /~_>---<__-      _-~\n");
+    printf("            ,/|           /__>--<__/     _-~\n");
+    printf("         ,//('(          |__>--<__|     /                  .----_\n");
+    printf("        ( ( '))          |__>--<__|    |                 /' _---_~\n");
+    printf("     `-)) )) (           |__>--<__|    | -IMPERATOR      /'  /     ~\\`\n");
+    printf("    ,/,'//( (             \__>--<__\    \            /'  //        ||\n");
+    printf("  ,( ( ((, ))              ~-__>--<_~-_  ~--____---~' _/'/        /'\n");
+    printf("`~/  )` ) ,/|                 ~-_~>--<_/-__       __-~ _/\n");
+  printf("._-~//( )/ )) `                    ~~-'_/_/ /~~~~~~~__--~\n");
+   printf(";'( ')/ ,)(                              ~~~~~~~~~~\n");
+  printf("' ) ( (/)\n");
+    printf("'   '  `\n");
+}
+
 
 void cavaloMarinho() {
     printf("                     $$$$..\n");
@@ -87,7 +132,7 @@ void cavaloMarinho() {
     printf("                 $$$$$\n");
     printf("                4$$$$$    .e.\n");
     printf("                ^$$$$$   dP*$L\n");
-    printf("                 $$$$$   %  4$\n");
+    printf("                 $$$$$      4$\n");
     printf("                 ^$$$$.     4$\n");
     printf("                  ^$$$$.   .$P\n");
     printf("                    *$$$$$$$$\n");
@@ -95,6 +140,80 @@ void cavaloMarinho() {
 
     sleepcp(2500);
     system("clear");
+}
+
+void gameStart(struct JOGADOR jogador) {
+    fase1(jogador);
+}
+
+
+
+void fase1(struct JOGADOR jogador) {
+    printf("FASE 1...\n\n");
+    cout << "Status do jogador\nNome: " << jogador.nome <<"\nVida: " << jogador.vida << "\nDano: " << jogador.dano << "\n" << endl;
+    loading();
+    printf("Você é um guerreiro que está em uma jornada atrás do herói caído Imperator que antes fora um paladino, mas após cometer o crime de matar o rei, tornou-se um algoz\n");
+    imperator();
+    sleepcp(3000);
+    
+	system("clear");
+    printf("Inicialmente você se encontra na entrada da cidade de Tristam. A cidade está vazia, de noite, escondido no escuro, uma criatura o observa...\n");
+    system("clear");
+
+    actionFase1(jogador);
+
+    sleepcp(1500);
+    system("clear");
+
+    if (jogador.vida > 0) {
+        fase2(jogador);
+    }
+}
+
+
+void actionFase1(struct JOGADOR jogador) {
+    int opcao;
+    
+    struct INIMIGO wolf;
+    wolf.tipo = "Bestial";
+
+    cout << "1. Observar criatura mais de perto\n" << endl << "2. Ignorar e seguir em sua jornada" << endl;
+    cin >> opcao;
+    switch (opcao) {
+        case 1:
+            int c;
+            printf("A criatura se mostra um lobo selvagem e avança em sua direção");
+            system("clear");
+            sleepcp(2000);
+            lobo();
+            //OPORTUNIDADE DE ESCOLHA ENTRE BATALHAR E FUGIR
+
+            cout << "1. Batalhar" << endl << "2. Fugir" << endl;
+            cin >> c;
+
+            if(c == 1) {
+                //DIRETO PRA BATALHA
+                system("clear");
+                batalha(jogador, wolf, 7);
+                break;
+          
+            } else {
+                bool flag = fugir();
+                //CASO O JOGADOR ESCOLHA FUGIR
+                if(flag == 1) {
+                    tentaFugir(flag);
+                    fase2(jogador); 
+                } else {
+                    tentaFugir(flag);
+                    batalha(jogador, wolf, 100);
+                }
+                
+            }
+            
+
+            
+    }
+
 }
 
 void fase2(struct JOGADOR jogador) {
@@ -109,25 +228,50 @@ void fase2(struct JOGADOR jogador) {
     system("clear");
 }
 
-void fase1(struct JOGADOR jogador) {
-    printf("FASE 1...\n\n");
-    cout << "Status do jogador\nNome: " << jogador.nome <<"\nVida: " << jogador.vida << "\nDano: " << jogador.dano << endl;
-    loading();
-    printf("História até o aparecimento de um inimigo qualquer...\n");
-    sleepcp(2500);
-    system("clear");
-    lobo();
-    jogador.vida -= 100;
-    //sleepcp(2500);
-    system("clear");
-    if (jogador.vida > 0) {
-        fase2(jogador);
+
+ void tipo_inimigo(struct INIMIGO inimigo) {
+    
+    if(inimigo.tipo == "Bestial") {
+        inimigo.vida = 70;
+        inimigo.dano = 15;
+    }   
+
+    else if(inimigo.tipo == "Soldado") {
+        inimigo.vida = 110;
+        inimigo.dano = 20;
+    }
+    
+    else {
+        inimigo.tipo = "Chefe";
+        inimigo.vida = 230;
+        inimigo.dano = 25;
+    }
+
+}
+
+void batalha(struct JOGADOR jogador,struct INIMIGO inimigo, int eIniciativa) {
+    tipo_inimigo(inimigo);
+    int init = rolaDado();
+    
+    bool firstAttack = iniciativa(init, eIniciativa);
+
+    if (firstAttack) {
+        //PARTE DO QUIZ E BONIFICAÇÃO
+    } else {
+        jogador.vida = jogador.vida - inimigo.dano;
     }
 }
 
-void gameStart(struct JOGADOR jogador) {
-    fase1(jogador);
+bool iniciativa(int iniciativa, int eIniciativa) {
+    return iniciativa > eIniciativa;
 }
+
+
+int rolaDado() {
+    int dado = (rand() % ( 20 - 1 )) + 1;
+    return dado;
+}
+
 
 string nomeDoJogador() {
     printf("ESCOLHA O NOME DO JOGADOR: ");
@@ -141,12 +285,25 @@ string nomeDoJogador() {
 void logoMenu() {
     vector<string> menu{"######  #       ######        #    ######  #     # ####### #     # ####### \n", "#     # #       #     #      # #   #     # #     # #       ##    #    #    \n", "#     # #       #     #     #   #  #     # #     # #       # #   #    #    \n", "######  #       ######     #     # #     # #     # #####   #  #  #    #    \n", "#       #       #          ####### #     #  #   #  #       #   # #    #    \n", "#       #       #          #     # #     #   # #   #       #    ##    #    \n", "#       ####### #          #     # ######     #    ####### #     #    #  \n\n\n"};
 
-    for (int i = 0; i < menu.size(); i++) {
+    for (unsigned i = 0; i < menu.size(); i++) {
         cout << menu[i];
         sleepcp(200);
     }
 }
 
+
+bool fugir() {
+    int rolagem = rolaDado();
+    return rolagem >= 15;
+}
+
+string tentaFugir(bool fugiu) {
+    string str =  "Você tenta fugir e... ";
+
+    (fugiu) ? str += "Escapou !" : str += "Não conseguiu ! O monstro irá usar essa brecha como oportunidade.";
+
+    return str;
+}
 
 int menu () {
 
@@ -156,19 +313,16 @@ int menu () {
     switch (opcao) {
     case 1: {
         sleepcp(1000);
-        string player = nomeDoJogador();
+        string playerName = nomeDoJogador();
         struct JOGADOR personagem;
-        personagem.nome = player;
-        cout << "Vamos lá " + player + "!" << endl;
+        personagem.nome = playerName;
+        cout << "Vamos lá " + playerName + "!" << endl;
         sleepcp(2000);
         system("clear");
         gameStart(personagem);
         break;
     }
     case 2:
-        sleepcp(2000);
-        system("clear");
-        showRanking();
         break;
     case 3:
         break;
@@ -198,3 +352,7 @@ int main() {
 
     return 0;
 }
+
+
+
+
