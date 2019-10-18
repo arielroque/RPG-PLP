1874028e2e7f1fe52a6e4cec9b30e61eed8e347e
igorlimam <igorlimam@gmail.com>
2019-10-18 12:39:56 -0300

    Linkagem completa que funcionou no meu pc e.e

--- haskell/Linker.hs
+++ haskell/Linker.hs
@@ -3,15 +3,26 @@ import Leitor_arquivo
 import Constantes
 import Util
 import Batalha2
+import Jogador
+import Inimigos
+import System.Exit
 import qualified System.Process as SP
 
-irParaTaverna::IO ()
-irParaTaverna = do
+irParaTaverna::[Int]->IO ()
+irParaTaverna went = do
     ret<-lerArquivo arquivoEntradaTaverna 1
     putStrLn ret
     opcao<-readLn::IO Int
-    if(opcao == 1) then putStrLn $ (show lerArquivo arquivoDescansar 1)
-    else putStrLn $ (show lerArquivo arquivoAproveitarNoite 1)
+    clrScreen
+    if(opcao == 1) then do
+
+        readShowAndClear arquivoDescansar
+        escolherCaminhoCidade went
+    else if(opcao == 3) then do
+        escolherCaminhoCidade went
+    else do
+        readShowAndClear arquivoAproveitarNoite
+        escolherCaminhoCidade went
 
 readShowAndClear::String->IO ()
 readShowAndClear path = do
@@ -22,11 +33,11 @@ readShowAndClear path = do
 
 concluirFase::[Int]->Int->[Int]->[Int]
 concluirFase [] ind res = reverse (1:res)
-concluirFase (x:xs) ind 0 = concluirFase xs (1:res) (-1)
-concluirFase (x:xs) ind res = concluirFase xs (x:res) (res-1)
+concluirFase (x:xs) 0 res = concluirFase xs (-1) (1:res)
+concluirFase (x:xs) ind  res = concluirFase xs (ind-1) (x:res)
 
 
-irParaPicosStress::Int->IO()
+irParaPicosStress::[Int]->IO()
 irParaPicosStress went = do
 
     --ja concluiu esse lugar
@@ -37,9 +48,9 @@ irParaPicosStress went = do
         readShowAndClear arquivoPicosStress0
         
         --Batalha minotauro alemao
-        player <- guerreiro
-        inimigo <- minotauro
-        batalha player inimigo rolaDado(20)
+        let player = guerreiro
+        let inimigo = minotauro
+        let resultadoInimigo = batalha player inimigo (rolaDado(20))
 
         readShowAndClear arquivoPicosStress1
         
@@ -48,30 +59,85 @@ irParaPicosStress went = do
         else readShowAndClear bossKazejin1
 
         --BATALHA CONTRA MAGO DE VENTO
-        boss <- magoVento
-        batalha player boss rolaDado(20)
+        let boss = magoVento
+        let resultadoBoss = batalha player boss (rolaDado(20))
 
         readShowAndClear arquivoPicosStress2
 
         escolherCaminhoCidade (concluirFase went 0 [])
 
+irParaEstradas::[Int]->IO ()
+irParaEstradas went = do
+    if(went!!1 == 1) then do 
+        readShowAndClear arquivoCaminhoConcluido
+        escolherCaminhoCidade went
+    else do
+        readShowAndClear arquivoEstradas1
+
+        --BATALHA CONTA MAGO DE FOGO
+        let boss = magoFogo
+        let player = guerreiro
+        let resultadoBoss = batalha player boss (rolaDado(20))
+
+        readShowAndClear arquivoEstradas2
+
+        escolherCaminhoCidade (concluirFase went 1 [])
+
+irParaLagoaGarcas::[Int]->IO ()
+irParaLagoaGarcas went = do
+    if(went!!2 == 1) then do
+        readShowAndClear arquivoCaminhoConcluido
+        escolherCaminhoCidade went
+    else do
+        if(went!!0 == 1 || went!!1 == 1) then do
+            --fase
+            readShowAndClear arquivoLagoaGarca1
+
+            --BATALHA XAMA NOOB
+            let inimigo = xamaNoob
+            let player = guerreiro
+            let resultadoBatalha = batalha player inimigo (rolaDado(20))
+
+            readShowAndClear arquivoLagoaGarca2
+
+            escolherCaminhoCidade (concluirFase went 2 [])
+
+        else do
+            --retorne
+            readShowAndClear arquivoLagoaGarca0
+            escolherCaminhoCidade went
+
+irParaTemploPythano::[Int]->IO ()
+irParaTemploPythano went = do
+    if(went!!0 == 1 && went!!1 == 1 && went!!2 == 1) then do
+
+        readShowAndClear arquivoTemplo1
+
+        --JOGO ACABOU
+
+    else do
+        --cannot enter
+        readShowAndClear arquivoTemplo0
+        escolherCaminhoCidade went
+
+
 {- --A estrategia eh ter uma lista dos lugares ja visitados-}
-ecolherCaminhoCidade::[Int]->IO ()
+escolherCaminhoCidade::[Int]->IO ()
 escolherCaminhoCidade went = do
 
     ret<-lerArquivo arquivoPartida 1
     
+    putStrLn ret
+
     opcao<-readLn::IO Int
     
     clrScreen
-    if(opcao == 0) then do irParaTaverna
-    else do irParaPicosStress went 0
-    {-if(opcao == 0) then irParaTaverna
-    else if(opcao == 1) then irParaPicoStress
-    else if(opcao == 2) then irParaEstradas
-    else if(opcao == 3) then irParaLagoGarcas
-    else if(opcao == 4) then irParaTemploPythano
-    else exitWith ExitSuccess-}
+    if(opcao == 0) then irParaTaverna went
+    else if(opcao == 1) then irParaPicosStress went
+    else if(opcao == 2) then irParaEstradas went
+    else if(opcao == 3) then irParaLagoaGarcas went
+    else if(opcao == 4) then irParaTemploPythano went
+    else exitWith ExitSuccess
     
 
 iniciarJogo::IO ()
--- haskell/constantes.hs
+++ haskell/constantes.hs
@@ -15,6 +15,13 @@ arquivoPicosStress2,
 bossKazejin1,
 bossKazejin2,
 arquivoCaminhoConcluido,
+arquivoEstradas1,
+arquivoEstradas2,
+arquivoLagoaGarca0,
+arquivoLagoaGarca1,
+arquivoLagoaGarca2,
+arquivoTemplo0,
+arquivoTemplo1,
 )where
 pathw = "input.txt"
 pathr = "input.txt"
@@ -32,3 +39,11 @@ arquivoPicosStress2 = "historia/Picos de stress 2.txt"
 bossKazejin1 = "historia/Mago de vento 1.txt"
 bossKazejin2 = "historia/Mago de vento 2.txt"
 arquivoCaminhoConcluido = "historia/Caminhos concluidos.txt"
+arquivoEstradas1 = "historia/Estrada da quintura 1.txt"
+arquivoEstradas2 = "historia/Estrada da quintura 2.txt"
+arquivoLagoaGarca0 = "historia/Lagoa das garcas 0.txt"
+arquivoLagoaGarca1 = "historia/Lagoa das garcas 1.txt"
+arquivoLagoaGarca2 = "historia/Lagoa das garcas 2.txt"
+arquivoTemplo0 = "historia/Templo dos Pythanos 0.txt"
+arquivoTemplo1 = "historia/Templo dos Pythanos 1.txt"
+
