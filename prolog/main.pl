fefc58ef749a941741079b98d94d8fb122d45442
ariel <ariel.roque@hotmail.com>
2019-11-16 15:48:59 -0300

    Adicionado perguntas e validação de respostas

--- prolog/main.pl
+++ prolog/main.pl
@@ -41,6 +41,29 @@ loading:-
          ,writeln("####### ####### #     # ######  ### #     #  #####  ").
 
 
+validarResposta(X,X,"Resposta Correta").
+validarResposta(_,_,"Resposta Incorreta").
+
+#path teste = perguntas/fase1-1.txt
+
+mostrarPergunta(Path):-
+   open(Path,read,Arquivo),
+   read_line_to_string(Arquivo,Titulo),
+   read_line_to_string(Arquivo,Alternativa1),
+   read_line_to_string(Arquivo,Alternativa2),
+   read_line_to_string(Arquivo,Alternativa3),
+   read_line_to_string(Arquivo,AlternativaCorreta),
+   close(Arquivo),
+   writeln(Titulo),
+   writeln(Alternativa1),
+   writeln(Alternativa2),
+   writeln(Alternativa3),
+   write("Resposta:"),
+   read(AtomoResposta),
+   atom_string(AtomoResposta,Resposta),
+   validarResposta(Resposta,AlternativaCorreta,Julgamento),
+   write(Julgamento).
+
 menu:-
     writeln("|-----------------------------------------------------------------------------|")
    ,writeln("|                                 Menu                                        |")
